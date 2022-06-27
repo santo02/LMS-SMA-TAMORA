4d22e0fddf1eb32b78ccca70d088f70543a6fc5f
Santo Lamsar Harianja <santolamsar@gmail.com>
2022-06-27 16:50:55 +0700

    login UI

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -1,34 +1,68 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html>
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" type="text/css" href="{{ url('css/style.css') }}">
   </head>
+
   <body>
-    <div class="container" action="/login" method="post">
-        @csrf
-        <form class="px-4 py-3">
-          <div class="mb-3">
-            <label for="exampleDropdownFormEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleDropdownFormEmail1" placeholder="email@example.com" autofocus required>
+    <div class="container-fluid">
+      <div class="row">
+        <!-- Desain kiri -->
+        <div class="col-md-5 left">
+          <div brand></div>
+        </div>
+        <div class="col-md-1"></div>
+        <!-- form kanan -->
+        <div class="col-md-5 kanan">
+          <div brand>
+            <img class="logo img-responsive" src="image/logo.png" alt="logo" />
           </div>
-          <div class="mb-3">
-            <label for="exampleDropdownFormPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleDropdownFormPassword1" placeholder="Password" autofocus required>
-          </div>
-          <div class="mb-3">
-            <div class="form-check">
-              <input type="checkbox" class="form-check-input" id="dropdownCheck" >
-              <label class="form-check-label" for="dropdownCheck">
-                Remember me
-              </label>
+          <h2>Login</h2>
+          <form class="needs-validation">
+            <div class="mb-3">
+              <label for="email" class="form-label"
+                >Email Address<div class="asterisk">*</div></label
+              >
+              <input
+                type="email"
+                class="form-control form-control-lg"
+                id="email"
+                placeholder="example@domain.com"
+                aria-describedby="emailHelp"
+              />
             </div>
-          </div>
-          <button type="submit" class="btn btn-primary">Masuk</button>
-        </form>
+            <div class="mb-3">
+              <label for="password" class="form-label"
+                >Password<div class="asterisk">*</div></label>
+              <input
+                type="password"
+                class="form-control form-control-lg"
+                id="password"
+                placeholder="password"
+                aria-describedby="password"
+              />
+            </div>
+            <button type="submit" id="button" class="btn btn-warning">
+              <strong>Submit</strong>
+            </button>
+          </form>
+        </div>
+        <div class="col-md-1"></div>
+      </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+      crossorigin="anonymous"
+    ></script>
   </body>
 </html>
