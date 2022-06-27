d8301cd5688afb6b77533d4c28460c6845fbbb67
Santo Lamsar Harianja <santolamsar@gmail.com>
2022-06-27 14:05:34 +0700

    first commit

---
+++ database/migrations/2022_06_27_063104_theacers.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('theacers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('NIP')->unique();
+            $table->string('phone');
+            $table->string('address');
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
+        integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="{{ url('css/lanpe.css') }}">
+    <title>Landing Page</title>
+</head>
+
+<body>
+    <div class="headers">
+        <img src="image/logo.png" alt="" class="img-responsive logo">
+    </div>
+    <div class=" float-right">
+        <a href={{"/login"}}><button class="btn text-uppercase text-white login">Login</button></a>
+    </div>
+    <div class="head">
+        <h4 class="text-white">LMS SMAN 1 TANJUNG MORAWA</h4>
+        <p class="mt-5">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse aliquam nunc non enim ultricies
+            pretium.
+        </p>
+        <button class="btn text-uppercase text-white more mt-3">More</button>
+    </div>
+    <div class=" img-responsive">
+        <img src="image/gambar1.png" class="gambar">
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
